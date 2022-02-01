effabfc545b35229a3ead4811d5e26c002fc4c73
iyo-bemoore <imadyoussoufi@gmail.com>
2022-02-01 22:36:54 +0100

    fix add to cart button - TODO: Fix cart action

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,17 +1,19 @@
-import StripeCheckout from 'react-stripe-checkout';
-import {stripeAPIKeyFE } from '../config/default.json'
+import StripeCheckout from "react-stripe-checkout";
+// import { stripeAPIKeyFE } from "../../../config/default.json";
 
-const STRIPE_PUBLISHABLE = stripeAPIKeyFE;
+const STRIPE_PUBLISHABLE =
+  "sk_test_51KLjm9JuqXwK9nyivlu0CAsq0CA4aWcXqcN3xvjDIRRuQjxT9qhKVbaJIikuerqHneLhlpoYKjq2ulKDLWMFOY9H00UWMgzW9y";
 
-const onToken = (user,checkout, history) => token => 
-    checkout(user, token.id, history);
+const onToken = (user, checkout, history) => (token) =>
+  checkout(user, token.id, history);
 
-const Checkout = ({ amount, user, checkout, history }) => 
-    <StripeCheckout
-      amount={amount*100}
-      token={onToken(user,checkout, history)}
-      currency='eur'
-      stripeKey={STRIPE_PUBLISHABLE}
-/>
+const Checkout = ({ amount, user, checkout, history }) => (
+  <StripeCheckout
+    amount={amount * 100}
+    token={onToken(user, checkout, history)}
+    currency="eur"
+    stripeKey={STRIPE_PUBLISHABLE}
+  />
+);
 
 export default Checkout;
--- client/src/components/Tickets.js
+++ client/src/components/Tickets.js
@@ -1,94 +1,106 @@
-import { Component } from 'react';
-import AppNavbar from './AppNavbar';
-import {Card, CardText, CardBody, CardTitle, CardSubtitle, Button, Container} from 'reactstrap';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
-import { addToCart, getCart } from '../actions/cartActions';
-
+import { Component } from "react";
+import AppNavbar from "./AppNavbar";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container,
+} from "reactstrap";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getItems } from "../actions/itemActions";
+import { addToCart, getCart } from "../actions/cartActions";
 
 class Tickets extends Component {
-    
-    state= {
-        loaded: false
-    }
+  state = {
+    loaded: false,
+  };
 
-    componentDidMount(){
-        this.props.getItems();   
-    }
+  componentDidMount() {
+    this.props.getItems();
+  }
 
-    static propTypes = {
-        getItems: PropTypes.func.isRequired,
-        item: PropTypes.object.isRequired,
-        isAuthenticated: PropTypes.bool,
-        addToCart: PropTypes.func.isRequired,
-        user: PropTypes.object.isRequired
-    }
-    
-    isItemInCart = (itemId, cartItems)  =>{
-        const itemInCart = cartItems.find( cartItem=> cartItem._id === itemId);
-        if(itemInCart){
-            return 'Added to cart'
-        }
-        return 'Add to cart'
-    }
+  isItemInCart = (itemId, cartItems) => {
+    console.log("itemId", itemId);
+    console.log("cartItems", cartItems);
 
-    onAddToCart = async (id, productId) => {
-        await this.props.addToCart(id, productId, 1);
-       
+    const itemInCart = cartItems.find(
+      (cartItem) => cartItem.productId === itemId
+    );
+    console.log("item in cart", itemInCart);
+    if (itemInCart) {
+      return "Added to Cart";
     }
+    return "Add To Cart";
+  };
 
-    getCartItems = async (id) => {
-        
-        await this.props.getCart(id);
-        this.state.loaded = true
-    }
+  onAddToCart = async (id, productId) => {
+    await this.props.addToCart(id, productId, 1);
+  };
 
-    render(){
-        const user = this.props.user;
-        const { items } = this.props.item;
-        
-        
-        if(this.props.isAuthenticated && !this.props.cart.loading && !this.state.loaded){
-            this.getCartItems(user._id);
-        }
-        console.log('cart from tickets',this.props.cart)
-
-        return (
-            <div>
-            <AppNavbar/>
-            <Container>
-                <div className="row">
-                {items.map((item)=>(
-                    <div className="col-md-4">
-                    <Card className="mb-4">
-                        <CardBody>
-                            <CardTitle tag="h5">{item.title}</CardTitle>
-                            <CardSubtitle tag="h6"> {item.price}€</CardSubtitle>
-                            <CardText>{item.category}</CardText>
-                            {this.props.isAuthenticated ? 
-                                <Button
-                                    color="success"
-                                    size="sm"
-                                    onClick={this.onAddToCart.bind(this, user._id, item._id)}
-                                    >{ this.isItemInCart(item._id, this.props.cart.items).bind(this) }</Button> :
-                                    null}
-                        </CardBody>
-                    </Card>
-                    </div>
-                ))}
-                 </div>
-            </Container>
-            </div>
-        )
-    }
+  render() {
+    const user = this.props.user;
+    const { items } = this.props.item;
+    console.log("From Tickets", this.props);
+    return (
+      <div>
+        <AppNavbar />
+        <Container>
+          <div className="row">
+            {items.map((item) => (
+              <div key={item.title} className="col-md-4">
+                <Card className="mb-4">
+                  <CardBody>
+                    <CardTitle tag="h5">{item.title}</CardTitle>
+                    <CardSubtitle tag="h6"> {item.price}€</CardSubtitle>
+                    <CardText>{item.category}</CardText>
+                    {this.props.isAuthenticated ? (
+                      <Button
+                        color="success"
+                        size="sm"
+                        onClick={this.onAddToCart.bind(
+                          this,
+                          user._id,
+                          item._id
+                        )}
+                      >
+                        {!this.props.cart.cart
+                          ? "Add To Cart"
+                          : this.isItemInCart(
+                              item._id,
+                              this.props.cart.cart.items
+                            )}
+                      </Button>
+                    ) : null}
+                  </CardBody>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  static propTypes = {
+    getCart: PropTypes.func.isRequired,
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool,
+    addToCart: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+  };
 }
 
 const mapStateToProps = (state) => ({
-    item: state.item,
-    isAuthenticated: state.auth.isAuthenticated,
-    user: state.auth.user,
-    cart: state.cart
-})
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user,
+  cart: state.cart,
+});
 
-export default connect(mapStateToProps, {getItems, addToCart, getCart})(Tickets);
+export default connect(mapStateToProps, { getItems, addToCart, getCart })(
+  Tickets
+);
