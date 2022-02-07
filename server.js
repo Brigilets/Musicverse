62b2c4f7fcfbcd22160c6d6c2deb8351fc6c8626
Brigilets <b.sabutyte@gmail.com>
2022-02-07 12:10:33 +0100

    setup for deploy

--- server.js
+++ server.js
@@ -18,10 +18,14 @@ app.use("/order", orderRoutes);
 
 //production to serve client files
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+}else {
+  app.get("/", (req,res)=>{
+    res.send('Api is running')
+  })
 }
 
 const dbURI = config.get("dbURI");
